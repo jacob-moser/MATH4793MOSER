dac7be9f997553e87c0e22c8d0cbd7022595b208
Jacob Moser <jacob.moser@ou.edu>
2021-03-29 14:48:44 -0500

    Final edits on MV Normality assignment

--- R/mvnormalityapp.R
+++ R/mvnormalityapp.R
@@ -1,28 +1,15 @@
-#' @title mvnormalityapp
+#' mvnormalityapp
 #'
-#'
-#'
-#' @description basic shiny dashboard that produces some plots and checks related to multivariate normality
+#' @description basic shiny dashboard that generates some output about multivariate nromality checks
 #'
 #' @details  User has ability to change plot views with dynamic inputs
-#' @return  2 plots
+#' @return  Normal proportion test outcomes, violin plots, standard normal and chi squared QQ plots, a confidence ellipse
 #'
-#' @section First Plot:
-#' The first plot allows the user to generate a scatterplot on two variables and further manipulate the size and coloring of the plot with additional input.
-#'
-#' Clicking the plot allows the user to calculate and display a drop-one sample correlation.
-#'
-#' @section Second Plot:
-#' The second plot generates the same scatterplot as the first (on the same variables) and allows the user to visualize a rotation of the data by rotated axes.
-#'
-#' The rotated sample correlation coefficient is calculated and displayed as well as a zeroing value.
-#'
-#' One limitation is that the scaling of the plot can result in skewed axes rotations. Occurs if range of X and Y variables are significantly different.
 #'
 #' @export
 #'
 #' @examples
 #' \dontrun{ mvnormalityapp()}
-firstshinyapp<-function(){
+mvnormalityapp <- function(){
   shiny::runApp(system.file("mvnormalityapp", package="MATH4793MOSER"),launch.browser = TRUE)
 }
